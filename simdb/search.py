4668bd9674434f2222025d8b3a17ac601abae933
christopher <cjwright4242@gmail.com>
2016-07-06 00:53:32 -0400

    WIP: add a bunch of schema, need to finalize the simulation loader Next move is to sort out the storage and retrieval tests, and write the simulation workflow/queue setup.

--- simdb/search.py
+++ simdb/search.py
@@ -9,6 +9,44 @@ import importlib
 __author__ = 'christopher'
 
 
+def build_obj(mod, obj_name, args=None, kwargs=None):
+    """
+    Build an object from some basic information.
+    Note if any of the kwarg keys are valid uuids we will attempt to load the
+    information in as a filestore retrieval
+    Parameters
+    ----------
+    mod: str
+        Module name/path
+    obj_name: str
+        The name of the object within the module
+    args: list or tup
+        The args to be passed to the object
+    kwargs: dict
+        The kwargs to be passed to the object
+    Returns
+    -------
+    object:
+        The built object
+    """
+    # If experimental PES put in the exp, also modify the calcs themselves
+    # they may need to write their own scatter object
+    mod = importlib.import_module(mod)
+    obj = getattr(mod, obj_name)
+    if kwargs is not None:
+        for key in kwargs.keys():
+            if check_uuid(kwargs[key]):
+                kwargs[key] = fsc.retrieve(kwargs[key])
+    if kwargs is None and args is None:
+        return obj()
+    elif kwargs is None:
+        return obj(*args)
+    elif args is None:
+        return obj(**kwargs)
+    else:
+        return obj(*args, **kwargs)
+
+
 @_ensure_connection
 def find_atomic_config_document(**kwargs):
     atomic_configs = AtomicConfig.objects(__raw__=kwargs).order_by(
@@ -26,41 +64,16 @@ def find_1d_data_document(**kwargs):
         yield data_set
 
 
-supported_calculators = {
-    '1D': ['pyiid.calc.calc_1d', 'Calc1D', find_1d_data_document],
-    'Spring': ['pyiid.calc.spring_calc', 'Spring'],
-    'LAMMPS': ['ase.calculators.lammpslib', 'LAMMPSlib']
-}
-
-supported_ensembles = {
-    'NUTS': ['pyiid.sim.nuts_hmc', 'Calc1D'], #FIXME: Needs to be NUTS?
-}
-
-
-def build_calculator(calculator, calc_kwargs, target_data=None):
-    if calculator in supported_calculators.keys():
-        # If experimental PES put in the exp, also modify the calcs themselves
-        # they may need to write their own scatter object
-        mod = importlib.import_module(supported_calculators[calculator][0])
-        calc = getattr(mod, supported_calculators[calculator][1])
-        if target_data is not None:
-            exp, = supported_calculators[calculator][2](_id=target_data.id)
-            exp_data = exp.file_payload
-            return calc(target_data=exp_data, **calc_kwargs)
-        else:
-            return calc(**calc_kwargs)
-
-
 @_ensure_connection
 def find_calc_document(**kwargs):
     calculators = Calc.objects(__raw__=kwargs).order_by(
         '-_id').all()
     for calc in calculators:
         # build the calculator
-        return_calc = build_calculator(
-            calculator=calc.calculator,
-            calc_kwargs=calc.calc_kwargs,
-            target_data=calc.target_data
+        return_calc = build_obj(
+            mod=calc.module,
+            obj_name=calc.object_name,
+            kwargs=calc.kwargs
         )
         calc.payload = return_calc
         yield calc
@@ -72,17 +85,36 @@ def find_pes_document(**kwargs):
         '-_id').all()
     for pes in potential_energy_surfaces:
         calc_l = []
-        for calc_params in pes.calc_list:
+        for calc in pes.calcs:
             # build the calculator
-            calc = build_calculator(
-                calculator=calc_params.calculator,
-                calc_kwargs=calc_params.calc_kwargs,
-                target_data=calc_params.target_data
-            )
-            calc_l.append(calc)
-        pes.payload = MultiCalc(calc_list=calc_l)
+            calc = find_calc_document(_id=calc)
+            calc_l.append(calc.payload)
+        # If we only have one calculator just use that calc
+        if len(pes.calcs) == 1:
+            pes.payload = calc.payload
+        else:
+            pes.payload = MultiCalc(calc_list=calc_l)
         yield pes
 
+
+@_ensure_connection
+def find_ensemble_document(atoms, **kwargs):
+    ensembles = Ensemble.objects(__raw__=kwargs).order_by(
+        '-_id').all()
+    for e in ensembles:
+        # build the calculator
+        return_ensemble = build_obj(
+            mod=e.module,
+            obj_name=e.object_name,
+            args=atoms,
+            kwargs=e.kwargs
+        )
+        e.payload = return_ensemble
+        yield e
+
+
+def build_sim(sim):
+    pass
 
 @_ensure_connection
 def find_simulation_document(priority=False, **kwargs):
@@ -92,27 +124,5 @@ def find_simulation_document(priority=False, **kwargs):
     else:
         sims = Simulation.objects(__raw__=kwargs).order_by('-_id').all()
     for sim in sims:
+        # Should I build the simulation on the way out?
         yield sim
-
-
-def build_ensemble(ensemble, ensemble_kwargs, atoms):
-    if ensemble in supported_ensembles.keys():
-        # If experimental PES put in the exp, also modify the calcs themselves
-        # they may need to write their own scatter object
-        mod = importlib.import_module(supported_ensembles[ensemble][0])
-        ens = getattr(mod, supported_ensembles[ensemble][1])
-        return ens(atoms, ensemble_kwargs)
-
-@_ensure_connection
-def find_ensemble_document(atoms, **kwargs):
-    ensembles = Ensemble.objects(__raw__=kwargs).order_by(
-        '-_id').all()
-    for e in ensembles:
-        # build the calculator
-        return_ensemble = build_ensemble(
-            ensemble=e.ensemble,
-            ensemble_kwargs=e.calc_kwargs,
-            atoms=atoms
-        )
-        e.payload = return_ensemble
-        yield e
